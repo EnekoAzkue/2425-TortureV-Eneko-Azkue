b05a702b87dc54028cb250029ab6e35cfae5fadd
Eneko Azkue <eneko.azkue@ikasle.aeg,eus>
2025-02-03 13:50:11 +0100

    Todas las constantes: FPS, las flechas,...

---
+++ src/constants.js
@@ -0,0 +1,35 @@
+//Constants
+
+//Estados del juego
+export const Game = 
+{
+    INVALID:            0,
+    PLAYING:            1,
+    OVER:               2,
+};
+
+//Velocidad del juego
+export const FPS = 30;
+
+export const SpriteID =
+{
+    PLAYER:             0,
+    SPIDER:             1,
+    MONEY:              2,
+    HEART:              3,
+    WALL:               4,
+}
+
+//TileSet
+export const Tile = 
+{
+    SIZE_16: 0, //Sprites de 16*16    
+}
+
+export const Key = 
+{
+    UP:         38,
+    DOWN:       40,
+    RIGHT:      39,
+    LEFT:       37,
+}
